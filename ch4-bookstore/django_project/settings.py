3ddfe562a5aeb1dad500ab11c61e17f554379005
Ulugbek Erkinov <u.erkinov@inbox.ru>
2023-03-17 19:26:22 +0500

    ch6

--- ch4-bookstore/django_project/settings.py
+++ ch4-bookstore/django_project/settings.py
@@ -89,6 +89,8 @@ DATABASES = {
 
 AUTH_USER_MODEL = "accounts.CustomUser" # new
 
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
--- ch4-bookstore/django_project/urls.py
+++ ch4-bookstore/django_project/urls.py
@@ -18,5 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("pages.urls"))
+    #user management
+    path('accounts/', include("django.contrib.auth.urls")),
+    path('accounts/', include("accounts.urls")),
+    path('', include("pages.urls"))
 ]
